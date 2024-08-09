91d15d9dc824506bf264d8646f1a19bcae0137ff
Arthur Vickers <ajcvickers@hotmail.com>
2024-08-09 16:39:29 +0100

    Initial commit

---
+++ JsonBugs/UserSecretsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+public class UserSecretsExtensions
+{
+    public static string Read(string key)
+        => JsonSerializer.Deserialize<Dictionary<string, string>>(
+            File.ReadAllText(PathHelper.GetSecretsPathFromSecretsId(
+                typeof(UserSecretsExtensions).Assembly.GetCustomAttribute<UserSecretsIdAttribute>()!
+                    .UserSecretsId)))![key];
+}
